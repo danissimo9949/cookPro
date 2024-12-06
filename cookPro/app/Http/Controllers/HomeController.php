3bf62075155c75e7cc61267f6218461526027621
Ruslan485-cyber <vasap2694@gmail.com>
2024-12-06 18:26:29 +0200

    Bug fixes on main page

--- cookPro/app/Http/Controllers/HomeController.php
+++ cookPro/app/Http/Controllers/HomeController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('index');
+        $posts = Post::latest()->take(3)->get();
+        return view('index', compact('posts'));
     }
 }
--- cookPro/resources/views/index.blade.php
+++ cookPro/resources/views/index.blade.php
@@ -11,36 +11,21 @@
 
     <div class="container my-5">
         <div class="row">
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="https://via.placeholder.com/350x200" class="card-img-top" alt="Рецепт 1">
-                    <div class="card-body">
-                        <h5 class="card-title">Рецепт для завтрака</h5>
-                        <p class="card-text">Этот вкусный и питательный рецепт идеально подходит для утреннего приема пищи.</p>
-                        <a href="#" class="btn btn-primary">Читать далее</a>
+            @forelse ($posts as $post)
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="{{ $post->image ? asset('storage/' . $post->image) : 'https://via.placeholder.com/350x200' }}" 
+                             class="card-img-top" alt="{{ $post->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            <p class="card-text">{{ Str::limit($post->content, 100) }}</p>
+                            <a href="{{ route('blog.show', $post->id) }}" class="btn btn-primary">Читати</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="https://via.placeholder.com/350x200" class="card-img-top" alt="Рецепт 2">
-                    <div class="card-body">
-                        <h5 class="card-title">Рецепт для обеда</h5>
-                        <p class="card-text">Потрясающее блюдо, которое легко приготовить, но оно восхитительно вкусное!</p>
-                        <a href="#" class="btn btn-primary">Читать далее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="https://via.placeholder.com/350x200" class="card-img-top" alt="Рецепт 3">
-                    <div class="card-body">
-                        <h5 class="card-title">Рецепт для ужина</h5>
-                        <p class="card-text">Завершите свой день этим легким и вкусным ужином, который понравится всем!</p>
-                        <a href="#" class="btn btn-primary">Читать далее</a>
-                    </div>
-                </div>
-            </div>
+            @empty
+                <p>Поки немає доступних рецептів.</p>
+            @endforelse
         </div>
     </div>
 
